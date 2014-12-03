5d4e3ecfbebab667b0be793ab7c5f7818b6cd712
Andrew Chavez <adchavez@gmail.com>
2014-12-03 14:59:51 -0600

    Initial commit. Basic D3 treemap from example

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+
+    // Clean files from dist/ before build
+    clean: {
+      css: ["dist/*.css", "dist/*.css.map"],
+      js: ["dist/*.js", "dist/*.js.map"]
+    },
+
+    // Transpile LESS
+    less: {
+      options: {
+        sourceMap: true,
+        paths: ['bower_components/bootstrap/less']
+      },
+      prod: {
+        options: {
+          compress: true,
+          cleancss: true
+        },
+        files: {
+          "dist/style.css": "src/css/style.less"
+        }
+      }
+    },
+
+    // Run our JavaScript through JSHint
+    jshint: {
+      js: {
+        src: ['src/js/**.js']
+      }
+    },
+
+    // Use Uglify to bundle up a pym file for the home page
+    uglify: {
+      options: {
+        sourceMap: true
+      },
+      homepage: {
+        files: {
+          'dist/scripts.js': [
+            'bower_components/jquery/dist/jquery.js',
+            'bower_components/underscore/underscore.js',
+            'bower_components/d3/d3.js',
+            'src/js/main.js'
+          ]
+        }
+      }
+    },
+
+    // Watch for changes in LESS and JavaScript files,
+    // relint/retranspile when a file changes
+    watch: {
+      markup: {
+        files: ['index.php']
+      },
+      scripts: {
+        files: ['src/js/**.js'],
+        tasks: ['jshint', 'clean:js', 'uglify']
+      },
+      styles: {
+        files: ['src/css/**.less'],
+        tasks: ['clean:css', 'less']
+      }
+    }
+
+  });
+
+  // Load the task plugins
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.registerTask('default', ['jshint', 'clean', 'less', 'uglify']);
+
+};
---
+++ src/js/main.js
@@ -0,0 +1,103 @@
+(function($, _, d3) {
+
+  $(function() {
+
+    $.ajax({
+      dataType: 'json',
+      url: 'perry-hires.json',
+      success: function(data) {
+        var grouped = _.chain(data)
+          .groupBy(function(hire) {
+            return hire['Agency name'];
+          })
+          .map(function(hires, agency) {
+            return {
+              name: agency,
+              children: hires
+            };
+          })
+          .value();
+        drawChart({
+          name: "Perry hires",
+          children: grouped
+        });
+      }
+    });
+
+    var drawChart = function(data) {
+      var width = 1170,
+      height = 600;
+
+      var color = d3.scale.category20c();
+
+      var treemap = d3.layout.treemap()
+        .size([width, height])
+        .sticky(true)
+        .value(function(d) { return d['Annual salary']; });
+
+      var div = d3.select("#chart")
+        .style("position", "relative")
+        .style("width", width + "px")
+        .style("height", height + "px");
+
+      var node = div.datum(data).selectAll(".node")
+        .data(treemap.nodes)
+        .enter().append("div")
+        .attr("class", "node")
+        .call(position)
+        .style("background", function(d) {
+          if(!d.children) {
+            return color(d['Agency name']);
+          }
+        })
+        .classed('agency', function(d) {
+          if(d.children) {
+            return true;
+          }
+        })
+        .classed('hire', function(d) {
+          if(!d.children) {
+            return true;
+          }
+        })
+        .attr('data-name', function(d) {
+          if(!d.children) {
+            var name = d['First name'];
+            if(d.MI.length !== 0) {
+              name = name + '. ' + d.MI;
+            }
+            return name + ' ' + d['Last name'];
+          }
+        })
+        .attr('data-salary', function(d) {
+          if(!d.children) {
+            return d['Annual salary'];
+          }
+        })
+        .text(function(d) {
+          if(!d.children) {
+            return d['First name'][0] + '. ' + d['Last name'];
+          }
+        });
+
+      d3.selectAll("input").on("change", function change() {
+        var value = this.value === "count" ? function() { return 1; } : function(d) { return d['Annual salary']; };
+
+        node
+          .data(treemap.value(value).nodes)
+          .transition()
+          .duration(1500)
+          .call(position);
+      });
+
+      function position() {
+        this.style("left", function(d) { return d.x + "px"; })
+          .style("top", function(d) { return d.y + "px"; })
+          .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+          .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+      }
+    };
+
+  });
+
+}(jQuery, _, d3));
